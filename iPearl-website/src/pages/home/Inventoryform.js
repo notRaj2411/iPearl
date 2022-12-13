5bdabec146795d159a84028e681b1888ecd2771a
Kushagra Singh <f20190994@hyderabad.bits-pilani.ac.in>
2022-12-14 03:34:54 +0530

    Added Slots option

--- iPearl-website/src/pages/home/Inventoryform.js
+++ iPearl-website/src/pages/home/Inventoryform.js
@@ -1,87 +1,88 @@
-import { useState, useEffect } from 'react'
-import { useFirestore } from '../../hooks/useFirestore'
-import { useAuthContext } from '../../hooks/useAuthContext'
-
+import { useState, useEffect } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Inventoryform({ uid, displayName }) {
-    const [name, setName] = useState('')
-    const [amount, setAmount] = useState('')
-    const [map, setmap] = useState(new Map());
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [slot, setSlot] = useState("");
+  const [map, setmap] = useState(new Map());
 
+  const { addDocument, response } = useFirestore("samples");
+  const { user } = useAuthContext();
 
-    const { addDocument, response } = useFirestore('samples')
-    const { user } = useAuthContext()
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (map.get(amount) != 1) {
-            addDocument({
-                uid,
-                name,
-                amount,
-
-            })
-            //updatemap(amount,1)
-            //setbook(false)
-        }
-        else {
-
-            // setbook(true)
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (map.get(amount) != 1) {
+      addDocument({
+        uid,
+        name,
+        amount,
+        slot,
+      });
+    } else {
     }
+  };
 
-    const handleChange = event => {
-        console.log(event.target.value);
-        setName(event.target.value);
-    };
+  const handleChange2 = (event) => {
+    console.log(event.target.value);
+    setAmount(event.target.value);
+  };
 
-    const handleChange2 = event => {
-        console.log(event.target.value);
-        setAmount(event.target.value);
-    };
+  const handleChange3 = (event) => {
+    console.log(event.target.value);
+    setSlot(event.target.value);
+  };
 
-    useEffect(() => {
-        if (response.success) {
-            setName('')
-            setAmount('')
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+      setSlot("");
+    }
+  }, [response.success]);
 
-        }
-    }, [response.success])
-
-
-    return (
-        <>
-            <h3>Add Samples</h3>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Sample name:</span>
-                    <input
-                        type="text"
-                        required
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                    />
-
-                </label>
-                <label>
-                    <span>Box No:</span>
-                    <select value={amount} onChange={handleChange2}>
-                        <option value="">--Choose box number--</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                    </select>
-                </label>
-                {<button>Add</button>}
-
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h3>Add Samples</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Sample name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Box No:</span>
+          <select value={amount} onChange={handleChange2}>
+            <option value="">--Choose box number--</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+        </label>
+        <label>
+          <span>Slot No:</span>
+          <input
+            onChange={handleChange3}
+            type="number"
+            required
+            min="0"
+            max="100"
+            value={slot}
+          />
+        </label>
+        {<button>Add</button>}
+      </form>
+    </>
+  );
 }
--- iPearl-website/src/pages/home/SampleList.js
+++ iPearl-website/src/pages/home/SampleList.js
@@ -1,33 +1,29 @@
-import styles from './Home.module.css'
-import { useAuthContext } from '../../hooks/useAuthContext'
+import styles from "./Home.module.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function SampleList({ sample }) {
+  return (
+    <>
+      <h2 style={{ color: "green" }}>Sample Inventory</h2>
+      <br />
+      <div className="title">
+        <h4>Box</h4>
+        <h4>Slot</h4>
+        <h4>Sample</h4>
+      </div>
 
-    return (
-        <>
-            <h2 style={{ color: "green" }}>Sample Inventory</h2>
+      <ul className={styles.transactions}>
+        {sample.map((transaction) => (
+          <li key={transaction.id}>
+            <p className={styles.name}> {transaction.amount}</p>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+            <p className={styles.name}> {transaction.slot}</p>
+            <p className={styles.amount}>{transaction.name}</p>
             <br />
-            <div className='title'>
-                <h4 >Box</h4>
-                <h4 >Sample</h4>
-            </div>
-
-            <ul className={styles.transactions}>
-                {sample.map((transaction) => (
-                    <li key={transaction.id}>
-                        <p className={styles.name}> {transaction.amount}</p>
-                        <p className={styles.amount}>{transaction.name}</p>
-
-                        <br />
-
-
-                    </li>
-                ))}
-            </ul>
-            {console.log(sample)}
-
-
-        </>
-
-    )
+          </li>
+        ))}
+      </ul>
+      {console.log(sample)}
+    </>
+  );
 }
